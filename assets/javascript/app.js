7936083a4f88a830eb72556dc15e73f676ef8881
Paden Pace <padenpace@unknownc49a027486b2.att.net>
2017-03-04 17:31:01 -0600

    index.php composer.json added, and basic html written

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCluKF_ekDil2qRlG5VC8FDQw8YXUhEnZA",
+    authDomain: "pjp-rps.firebaseapp.com",
+    databaseURL: "https://pjp-rps.firebaseio.com",
+    storageBucket: "pjp-rps.appspot.com",
+    messagingSenderId: "60245522370"
+  };
+  firebase.initializeApp(config);
